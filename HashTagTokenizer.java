7c28cb3006e448b1ecb53ffd3f2b8f6ed5b66261
romerachlin <rome.rachlin@post.runi.ac.il>
2024-01-31 15:52:55 +0200

    Update HashTagTokenizer.java

--- HashTagTokenizer.java
+++ HashTagTokenizer.java
@@ -14,13 +14,19 @@ public class HashTagTokenizer {
 
 		In in = new In(fileName);
 
-		// Your code here
-
+		for (int i = 0; i < dictionary.length; i++) {
+			dictionary[i] = in.readLine();
+		}
 		return dictionary;
 	}
 
 	public static boolean existInDictionary(String word, String []dictionary) {
-		// Your code here
+		for (int i = 0; i < dictionary.length; i++) {	
+		if (word==dictionary[i]){
+			return true;
+			}
+		}
+		return false;
 	}
 
 	public static void breakHashTag(String hashtag, String[] dictionary) {
